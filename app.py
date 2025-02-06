4facac075526ae5de9be14b9ea3da52862743b1b
Ahmed Burhan <102169599+Juggy-45@users.noreply.github.com>
2025-02-06 23:03:04 +0000

    Update app.py

--- app.py
+++ app.py
@@ -12,7 +12,7 @@ load_dotenv()
 openai.api_key = os.getenv("OPENAI_API_KEY")
 
 app = Flask(__name__)
-CORS(app, origins=["http://localhost:5173"])
+CORS(app, origins=["https://structai.netlify.app/"])
 
 # Set upload folder
 UPLOAD_FOLDER = 'uploads'
